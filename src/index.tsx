f2128a484a60fccefdfe9124eae82426c8326358
wiseacre <303013435@qq.com>
2024-07-14 17:50:10 +0800

    bug修复

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import './index.css';
 import './App.css';
 
 // 连接到 Socket.IO 服务器 http://117.72.8.112:3000/  'http://192.168.50.44:3000'
-const socket: Socket = io('192.168.50.44:3000', {
+const socket: Socket = io('117.72.8.112:3000', {
   reconnectionAttempts: 5, // 重新连接尝试次数
   reconnectionDelay: 2000, // 重新连接延迟
   transports: ['websocket'],
